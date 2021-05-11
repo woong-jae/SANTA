a034afea83632581adf29ebd610acefd6c1974d1
bsu <bsu0541@naver.com>
2021-05-12 01:08:47 +0900

    add calendar

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import SearchIcon from "@material-ui/icons/Search";
 import Grid from "@material-ui/core/Grid";
+import SelectDate from "./Sections/SelectDate";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
-      margin: theme.spacing(1),
+      margin: theme.spacing(3),
       width: "300px",
     },
   },
@@ -26,11 +28,12 @@ export default function BasicTextFields() {
             label="산/지역명"
             variant="outlined"
           ></TextField>
-          <TextField
+          <SelectDate></SelectDate>
+          {/* <TextField
             id="outlined-basic"
             label="가고 싶은 날짜"
             variant="outlined"
-          ></TextField>
+          ></TextField> */}
           <TextField
             id="outlined-basic"
             label="동행 인원"
@@ -39,13 +42,17 @@ export default function BasicTextFields() {
             InputLabelProps={{ shrink: true }}
             variant="outlined"
           ></TextField>
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<SearchIcon />}
-          >
-            Search
-          </Button>
+          <br></br>
+          <Link to="/cardList">
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              startIcon={<SearchIcon />}
+            >
+              Search
+            </Button>
+          </Link>
         </form>
       </Grid>
     </div>
---
+++ client/src/components/LandingPage/Sections/SelectDate.js
@@ -0,0 +1,33 @@
+import "date-fns";
+import React from "react";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+
+export default function MaterialUIPickers() {
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <KeyboardDatePicker
+        disableToolbar
+        variant="inline"
+        format="yyyy-MM-dd"
+        margin="normal"
+        id="date-picker-inline"
+        label="가고 싶은 날짜"
+        value={selectedDate}
+        onChange={handleDateChange}
+        KeyboardButtonProps={{
+          "aria-label": "change date",
+        }}
+      />
+    </MuiPickersUtilsProvider>
+  );
+}
