70876562b5f648eeac750442b9ebf5ad426aae1d
e70e6916 <dgkim69166916@gmail.com>
2021-05-12 17:59:28 +0900

    Install react-bootstrap

---
+++ client/src/components/CardListPage/CardListPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Sections/CardListPage.scss";
+
+class CardListPage extends Component {
+  render() {
+    return (
+      <div className="cardList">
+        <header className="cardList-header"></header>
+        <section className="cardList-body"></section>
+      </div>
+    );
+  }
+}
+
+export default CardListPage;
