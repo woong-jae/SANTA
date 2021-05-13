798e41a5bcebadc6574f44efb69c18766582dc89
e70e6916 <dgkim69166916@gmail.com>
2021-05-13 19:22:43 +0900

    Create Search component

--- client/src/components/CardListPage/Sections/CardListHeader.js
+++ client/src/components/CardListPage/Sections/CardListHeader.js
@@ -1,35 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./CardListPage.scss";
-
+import Search from "../../common/Search";
 import { Button } from "@material-ui/core";
 import { AiOutlineUser } from "react-icons/ai";
-import SelectDate from "../../common/SelectDate";
-import TextField from "@material-ui/core/TextField";
-import SearchIcon from "@material-ui/icons/Search";
 
 class CardListHeader extends Component {
   render() {
     return (
       <header className="cardList-header">
-        <form>
-          <TextField label="산/지역명" className="header-input"></TextField>
-          <SelectDate className="header-input"></SelectDate>
-          <TextField
-            className="header-input"
-            label="동행 인원"
-            type="number"
-            defaultValue="0"
-            InputLabelProps={{ shrink: true }}
-          ></TextField>
-          <Link to="/list">
-            <Button
-              variant="contained"
-              startIcon={<SearchIcon />}
-              id="search-btn"
-            ></Button>
-          </Link>
-        </form>
+        <Search />
         <div>
           <Link to="/myPage">
             <Button variant="contained" className="header-btn" id="myPage-btn">
