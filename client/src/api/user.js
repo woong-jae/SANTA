9a7c03e840c05cd177d10bc78d7bc486744e8cc3
woong-jae <jwchung0828@gmail.com>
2021-05-17 13:30:07 +0900

    Update api

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const url = 'http"//localhost:5000/user';
 
 export const getUser = (_id) => axios.get(`${url}/${_id}`);
-export const loginUser = (user) => axios.post('/login', user);
+export const loginUser = (user) => axios.post(`${url}/login`, user);
 export const createUser = (user) => axios.post(url, user);
 export const updateUser = (_id, user) => axios.patch(`${url}/${_id}`, user);
 export const deleteUser = (_id) => axios.delete(`${url}/${_id}`);
