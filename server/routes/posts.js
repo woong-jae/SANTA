bf66a11ea43b44d7e4cb4911104d69294de0e0d0
Jfe <33208246+Go-Jaecheol@users.noreply.github.com>
2021-05-18 15:00:13 +0900

    Add getPostInfo

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { createPost, getPosts } from '../controllers/posts.js';
+import { createPost, getPosts, getPostInfo } from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.post('/', createPost);
 router.get('/', getPosts);
+router.get('/:_id', getPostInfo);
 
 export default router;
