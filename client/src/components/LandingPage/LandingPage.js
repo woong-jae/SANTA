69410c11148b98eff785be17763a03d7e423336a
e70e6916 <dgkim69166916@gmail.com>
2021-05-18 18:43:10 +0900

    Update LandingPage

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -24,6 +24,7 @@ const LandingPage = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    document.location.pathname = "/list";
     setSearchState(initialState);
   };
 
