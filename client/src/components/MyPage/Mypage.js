cf51ccb47c1b588ccf616cda9084126f52f478e4
e70e6916 <dgkim69166916@gmail.com>
2021-05-20 21:58:34 +0900

    Update MyPage

--- client/src/components/MyPage/Mypage.js
+++ client/src/components/MyPage/Mypage.js
@@ -1,9 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
+import CardListHeader from "../CardListPage/Sections/CardListHeader";
+import "./Sections/MyPage.scss";
 
-class MyPage extends Component {
-  render() {
-    return <h1>MyPage</h1>;
-  }
-}
+const MyPage = () => {
+  return (
+    <div className="mypage">
+      <CardListHeader />
+      <section className="mypage-body">
+        <article></article>
+      </section>
+      <footer></footer>
+    </div>
+  );
+};
 
 export default MyPage;
