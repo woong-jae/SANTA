7acf8f0a22b91aba3bd6055b95a4446726891662
bsu <bsu0541@naver.com>
2021-05-26 18:30:43 +0900

    sign form validate

--- client/src/components/common/check.js
+++ client/src/components/common/check.js
@@ -1,4 +1,9 @@
+export function isEmail(asValue) {
+  var regExp =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  return regExp.test(asValue); // 형식에 맞는 경우 true 리턴
+}
 export function isPassword(asValue) {
-    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,}$/; //  8 ~ 10자 영문, 숫자 조합
-    return regExp.test(asValue); // 형식에 맞는 경우 true 리턴
+  var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,15}$/; //  8 ~ 15자 영문, 숫자 조합
+  return regExp.test(asValue); // 형식에 맞는 경우 true 리턴
 }
