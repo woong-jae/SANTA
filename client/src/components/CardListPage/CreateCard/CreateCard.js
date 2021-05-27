4c141de9f27494303bdaf53f45e2689f35a0d6dd
woong-jae <jwchung0828@gmail.com>
2021-05-27 20:44:49 +0900

    Apply delete user

--- client/src/components/CardListPage/CreateCard/CreateCard.js
+++ client/src/components/CardListPage/CreateCard/CreateCard.js
@@ -41,17 +41,7 @@ export default function CreateCard(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-<<<<<<< HEAD
     dispatch(createPost({ ...cardState, createdUser: props.user?.result?._id})); // 새로운 post 생성 요청
-=======
-    dispatch(
-      createPost({
-        ...cardState,
-        createdUser: props.user?.result?.nickname,
-        currentMember: [props.user?.result?.nickname],
-      })
-    ); // 새로운 post 생성 요청
->>>>>>> 91988dfe68ba5dd3f333d88266bbd2db1cb5efe6
     setCardState(initialState);
     setAgeLimit([19, 70]);
     handleClose();
