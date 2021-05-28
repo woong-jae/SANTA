a27a642712ffb94cd195f388b7cbd825453d91a7
e70e6916 <dgkim69166916@gmail.com>
2021-05-28 21:34:17 +0900

    Create logo click function

--- client/src/components/CardListPage/Sections/CardListHeader.js
+++ client/src/components/CardListPage/Sections/CardListHeader.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { debounce } from "lodash";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 import { AiOutlineUser } from "react-icons/ai";
 import { Button } from "@material-ui/core";
@@ -10,7 +10,7 @@ import { Typography } from "@material-ui/core";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import Popover from "@material-ui/core/Popover";
 import SearchIcon from "@material-ui/icons/Search";
-import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandLess from "@material-ui/icons/ExpandLess";
 
 import SignPage from "../../SignPage/SignPage";
 import InputMountain from "../../common/InputMountain";
@@ -22,9 +22,9 @@ import "../../common/Sections/Search.scss";
 
 const useStyles = makeStyles((theme) => ({
   title: {
-    fontFamily: 'Pacifico',
-    fontSize: 40
-  }
+    fontFamily: "Pacifico",
+    fontSize: 40,
+  },
 }));
 
 const CardListHeader = (props) => {
@@ -109,9 +109,16 @@ const CardListHeader = (props) => {
 
   return (
     <header className="cardList-header">
-      <Typography className={classes.title} variant="h3" align="center" id="header-logo">
-        Santa
-      </Typography>{" "}
+      <Link to="/">
+        <Typography
+          className={classes.title}
+          variant="h3"
+          align="center"
+          id="header-logo"
+        >
+          Santa
+        </Typography>{" "}
+      </Link>
       {windowSize.width >= 1000 ? (
         <form onSubmit={handleSubmit} className="input-form">
           <div id="search-mountain" className="search-item">
@@ -148,8 +155,13 @@ const CardListHeader = (props) => {
               horizontal: "center",
             }}
           >
-            <form onSubmit={handleSubmit} style={{ textAlign: "center", paddingBottom: "13px"}}>
-              <Button onClick={handleClose}><ExpandLess /></Button>
+            <form
+              onSubmit={handleSubmit}
+              style={{ textAlign: "center", paddingBottom: "13px" }}
+            >
+              <Button onClick={handleClose}>
+                <ExpandLess />
+              </Button>
               <div className="input-form">
                 <div id="search-mountain" className="search-item">
                   <InputMountain name="mountain" handleChange={handleChange} />
