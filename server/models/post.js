1855c541b3c4d3b03775b019e2563729fb4da72d
bsu <bsu0541@naver.com>
2021-05-29 14:16:45 +0900

    mypage design

--- server/models/post.js
+++ server/models/post.js
@@ -1,38 +1,38 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-    title: String,
-    description: String,
-    mountain: String,
-    createdUser: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        require: true
-    },
-    contact: String,
-    maxMember: Number,
-    currentMember: {
-        type: [mongoose.Schema.Types.ObjectId],
-        ref: 'User',
-        default: []
-    },
-    ageLimit: {
-        type: [Number],
-        default: [1, 100]
-    },
-    date: {
-        type: Date,
-        default: new Date()
-    },
-    dueDate: Date
+  title: String,
+  description: String,
+  mountain: String,
+  createdUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  contact: String,
+  maxMember: Number,
+  currentMember: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "User",
+    default: [],
+  },
+  ageLimit: {
+    type: [Number],
+    default: [1, 100],
+  },
+  date: {
+    type: Date,
+    default: new Date(),
+  },
+  dueDate: Date,
 });
 
-postSchema.pre("findOneAndUpdate", function(next) {
-    if (this.currentMember.length + 1 > this.maxMember) {
-      throw Error;
-    }
-    next();
+postSchema.pre("findOneAndUpdate", function (next) {
+  if (this.currentMember.length + 1 > this.maxMember) {
+    throw Error;
+  }
+  next();
 });
 
-const Posts = mongoose.model('Posts', postSchema);
+const Posts = mongoose.model("Posts", postSchema);
 export default Posts;
