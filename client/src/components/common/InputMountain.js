5cb264aa2223c864c65c5423ff5ec7397178da93
Go-Jaecheol <gojaech@naver.com>
2021-05-31 00:32:43 +0900

    Fix search error

--- client/src/components/common/InputMountain.js
+++ client/src/components/common/InputMountain.js
@@ -21,20 +21,6 @@ const InputMountain = ({ results, keyword, updateField, getMountainValue, getKey
     setIsCorrectName(false);
   };
 
-  // var renderResults =
-  //   results &&
-  //   results.map(({ name, location }, index) => {
-  //     return (
-  //       <SearchPreview
-  //         key={index}
-  //         clickHandler={ClickHandler}
-  //         index={index}
-  //         name={name}
-  //         location={location}
-  //       />
-  //     );
-  //   });
-
   const defaultProps = {
     options: results,
     getOptionLabel: (option) => `${option.name} (${option.location})`,
@@ -68,30 +54,12 @@ const InputMountain = ({ results, keyword, updateField, getMountainValue, getKey
             onChange={ChangeHandler}
             //autoComplete="off"
             // helperText={!isCorrectName && "가고 싶은 산을 선택해주세요"}
-            // error={!isCorrectName && keyword !== ""}
+            error={!isCorrectName && keyword !== ""}
           ></TextField>
         )}
       ></Autocomplete>
-
-      {/* {results && results.length > 0 ? (
-        <div className="search-results">{renderResults}</div>
-      ) : null} */}
     </div>
   );
 };
-
-// const SearchPreview = ({ name, location, index, clickHandler }) => {
-//   return (
-//     <div
-//       onClick={() => clickHandler(name, location)}
-//       className={`search-preview ${index === 0 ? "start" : ""}`}
-//     >
-//       <div className="search-element">
-//         <p className="mntnm">{name}</p>
-//         <p className="location">{location}</p>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default InputMountain;
