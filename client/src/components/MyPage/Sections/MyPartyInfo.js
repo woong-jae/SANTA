285cdc22bbaa9b3140f90cdc1f471403c3c07a12
bsu <bsu0541@naver.com>
2021-06-02 00:53:57 +0900

    Update MyPage Design

---
+++ client/src/components/MyPage/Sections/MyPartyInfo.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+});
+
+const MyPartyInfo = (props) => {
+  const classes = useStyles();
+  const userCreatedPosts = useSelector((state) => state.mypage.created);
+  const userAppliedPosts = useSelector((state) => state.mypage.applied);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>모임 제목</StyledTableCell>
+            <StyledTableCell align="right">산 이름</StyledTableCell>
+            <StyledTableCell align="right">현재 인원</StyledTableCell>
+            <StyledTableCell align="right">날짜</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(props.type === "create"
+            ? userCreatedPosts
+            : userAppliedPosts).map((post) => (
+                <StyledTableRow key={post.title}>
+                  <StyledTableCell component="th" scope="row">
+                    {post.title}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {post.mountain}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {post.currentMember.length + 1 + "/" + post.maxMember}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {post.date.substring(0, 4) +
+                      "/" +
+                      post.date.substring(5, 7) +
+                      "/" +
+                      post.date.substring(8, 10)}
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default MyPartyInfo;
