0ba5f5b35831fbc196d0bf365a49ed85b6b46cda
bsu <bsu0541@naver.com>
2021-06-01 19:21:14 +0900

    working on mypage design

--- client/src/components/MyPage/MyPage.js
+++ client/src/components/MyPage/MyPage.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 import {
   Paper,
   Typography,
   Tooltip,
   Button,
-  Fab,
-  Divider,
-  Grid,
-  Container,
+  Tab,
+  Tabs,
+  Box,
+  AppBar,
 } from "@material-ui/core";
-import PersonRoundedIcon from "@material-ui/icons/PersonRounded";
+import PropTypes from "prop-types";
 import CreateIcon from "@material-ui/icons/Create";
-import GroupIcon from "@material-ui/icons/Group";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import RefreshIcon from "@material-ui/icons/Refresh";
 import decode from "jwt-decode";
 
 import { deleteUser, updateUser } from "../../actions/auth";
@@ -23,17 +25,52 @@ import UpdateUser from "./Sections/UpdateUser";
 import Cards from "../CardListPage/Sections/Cards";
 import "./Sections/MyPage.scss";
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`wrapped-tabpanel-${index}`}
+      aria-labelledby={`wrapped-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={2}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `wrapped-tab-${index}`,
+    "aria-controls": `wrapped-tabpanel-${index}`,
+  };
+}
+
 const MyPage = (props) => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const [isUpdate, setIsUpdate] = useState(false);
+  const [value, setValue] = useState("one");
   const userUpdated = useSelector((state) => state.auth.authData);
   const userCreatedPosts = useSelector((state) => state.mypage.created);
   const userAppliedPosts = useSelector((state) => state.mypage.applied);
 
   useEffect(() => {
-    if (user) { 
+    if (user) {
       dispatch(getUserPosts(user?.result?._id));
       dispatch(getUserAppliedPosts(user?.result?._id));
     }
@@ -85,112 +122,114 @@ const MyPage = (props) => {
     }
   };
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className="mypage">
       <CardListHeader user={user} />
-      {!isUpdate ? (
-        <div className="mypage-main">
-          <Paper className="mypage-paper" elevation={10}>
-            <section className="mypage-body">
-              <header>
-                <Typography
-                  className="title"
-                  variant="h3"
-                  style={{ textAlign: "center" }}
-                >
-                  <strong>My Page</strong>
-                </Typography>
-                <Tooltip title="내 모임 정보">
-                  <Fab href="#partyinfo" className="toggle-fab">
-                    <GroupIcon />
-                  </Fab>
-                </Tooltip>
-              </header>
-              <article>
-                <Typography>
-                  <div>
-                    <strong>이메일</strong>
-                  </div>
-                  <div id="email">{user?.result?.email}</div>
-                </Typography>
-                <hr />
-                <Typography>
-                  <div>
-                    <strong>닉네임</strong>
-                  </div>
-                  <div id="nickname">{user?.result?.nickname}</div>
-                </Typography>
-                <hr />
-                <Typography>
-                  <div>
-                    <strong>성별</strong>
-                  </div>
-                  <div id="sex">
-                    {user?.result?.sex === "male" ? "남성" : "여성"}
-                  </div>
-                </Typography>
-                <hr />
-                <Typography>
-                  <div>
-                    <strong>생년월일</strong>
-                  </div>
-                  <div id="birth">{birth}</div>
-                </Typography>
-                <hr />
-              </article>
-            </section>
-            <footer>
-              <Tooltip title="정보 변경">
-                <Button
-                  variant="contained"
-                  id="update-btn"
-                  onClick={isUpdateUser}
-                >
-                  <CreateIcon />
-                </Button>
-              </Tooltip>
-              <Button
-                variant="contained"
-                id="delete-btn"
-                onClick={handleDeleteUser}
-              >
-                회원 탈퇴
-              </Button>
-            </footer>
-          </Paper>
-        </div>
-      ) : isUpdate ? (
-        <UpdateUser
-          user={user}
-          updateUser={handleUpdateUser}
-          update={notUpdateUser}
-        />
-      ) : (
-        ""
-      )}
       <div className="mypage-main">
         <Paper className="mypage-paper" elevation={10}>
-          <section id="partyinfo" className="mypage-body">
+          <section className="mypage-body">
             <header>
+              <Button
+                variant="contained"
+                className="back-btn"
+                onClick={() => {
+                  history.goBack();
+                }}
+              >
+                <ArrowBackIcon />
+              </Button>
+              <Button variant="contained" className="refresh-btn">
+                <RefreshIcon />
+              </Button>
               <Typography className="title" variant="h3" align="center">
-                <strong>{"내 모임 정보"}</strong>
+                <strong>{"MY PAGE"}</strong>
               </Typography>
-              <Tooltip title="내 정보">
-                <Fab href="#" className="toggle-fab">
-                  <PersonRoundedIcon />
-                </Fab>
-              </Tooltip>
             </header>
-            <Typography
-              variant="h6"
-              align="center"
-              style={{ fontWeight: "800" }}
-              paragraph
-            >
-              {"생성한 모임"}
-            </Typography>
-            <Divider />
-            <article>
+            <AppBar position="static">
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="wrapped label tabs example"
+                centered
+              >
+                <Tab value="one" label="내 정보" {...a11yProps("one")}></Tab>
+                <Tab value="two" label="생성한 모임" {...a11yProps("two")} />
+                <Tab
+                  value="three"
+                  label="참가 신청한 모임"
+                  {...a11yProps("three")}
+                />
+              </Tabs>
+            </AppBar>
+            <TabPanel value={value} index="one">
+              {!isUpdate ? (
+                <section className="mypage-body">
+                  <article>
+                    <Typography>
+                      <div>
+                        <strong>이메일</strong>
+                      </div>
+                      <div id="email">{user?.result?.email}</div>
+                    </Typography>
+                    <hr />
+                    <Typography>
+                      <div>
+                        <strong>닉네임</strong>
+                      </div>
+                      <div id="nickname">{user?.result?.nickname}</div>
+                    </Typography>
+                    <hr />
+                    <Typography>
+                      <div>
+                        <strong>성별</strong>
+                      </div>
+                      <div id="sex">
+                        {user?.result?.sex === "male" ? "남성" : "여성"}
+                      </div>
+                    </Typography>
+                    <hr />
+                    <Typography>
+                      <div>
+                        <strong>생년월일</strong>
+                      </div>
+                      <div id="birth">{birth}</div>
+                    </Typography>
+                    <hr />
+                  </article>
+                  <footer>
+                    <Tooltip title="정보 변경">
+                      <Button
+                        variant="contained"
+                        id="update-btn"
+                        onClick={isUpdateUser}
+                      >
+                        <CreateIcon />
+                      </Button>
+                    </Tooltip>
+                    <Button
+                      variant="contained"
+                      id="delete-btn"
+                      onClick={handleDeleteUser}
+                    >
+                      회원 탈퇴
+                    </Button>
+                  </footer>
+                </section>
+              ) : isUpdate ? (
+                <UpdateUser
+                  user={user}
+                  updateUser={handleUpdateUser}
+                  update={notUpdateUser}
+                />
+              ) : (
+                ""
+              )}
+            </TabPanel>
+            <TabPanel value={value} index="two">
               <section>
                 <div className="cardList-body">
                   {userCreatedPosts.map((post) => (
@@ -198,17 +237,8 @@ const MyPage = (props) => {
                   ))}
                 </div>
               </section>
-            </article>
-            <Typography
-              variant="h6"
-              align="center"
-              style={{ fontWeight: "800" }}
-              paragraph
-            >
-              {"참가 신청한 모임"}
-            </Typography>
-            <Divider />
-            <article>
+            </TabPanel>
+            <TabPanel value={value} index="three">
               <section>
                 <div className="cardList-body">
                   {userAppliedPosts.map((post) => (
@@ -216,7 +246,7 @@ const MyPage = (props) => {
                   ))}
                 </div>
               </section>
-            </article>
+            </TabPanel>
           </section>
         </Paper>
       </div>
