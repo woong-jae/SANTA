26da4c9ac0c748aab5cc385dbc4b7087500eceea
woong-jae <jwchung0828@gmail.com>
2021-06-13 03:51:41 +0900

    Create ShowCardPage backdrop

--- client/src/components/ShowCardpage/ShowCardPage.js
+++ client/src/components/ShowCardpage/ShowCardPage.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import decode from "jwt-decode";
+import { makeStyles } from '@material-ui/core/styles';
 
 import { getPostById } from "../../actions/show";
 import Card from "./Sections/Card";
 import "./Sections/ShowCardPage.scss";
 
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
+
 export default function ShowCard() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const history = useHistory();
+  const classes = useStyles();
 
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const card = useSelector(state => state.show.post);
@@ -35,5 +46,9 @@ export default function ShowCard() {
   }, [dispatch, userUpdate]);
 
   if (card && card._id === id) return (<Card user={user} card={card} />);
-  else return <div></div>
+  else return (
+    <Backdrop className={classes.backdrop} open={true}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  )
 }
