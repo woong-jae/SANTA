f11346c67731da9c5cf6cfe672aa3d14e588ba39
e70e6916 <dgkim69166916@gmail.com>
2021-06-12 19:15:52 +0900

    Update MountainDetail

--- client/src/components/ShowCardpage/Sections/MountainDetail.js
+++ client/src/components/ShowCardpage/Sections/MountainDetail.js
@@ -5,7 +5,6 @@ import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import MuiDialogContent from "@material-ui/core/DialogContent";
-import MuiDialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
@@ -48,13 +47,6 @@ const DialogContent = withStyles((theme) => ({
     padding: theme.spacing(2),
   },
 }))(MuiDialogContent);
-
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
 
 export default function MountainDetail(props) {
   const [open, setOpen] = React.useState(false);
