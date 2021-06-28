d9b7a93f615c669eb235f03947a91fe78e8d29a9
woong-jae <jwchung0828@gmail.com>
2021-06-28 19:33:21 +0900

    Update mypage reducer

--- client/src/reducers/mypage.js
+++ client/src/reducers/mypage.js
@@ -1,11 +1,13 @@
-import { FETCH_CREATED, FETCH_APPLIED } from '../constants/actionTypes';
+import { FETCH_CREATED, FETCH_APPLIED, FETCH_ACCEPTED } from '../constants/actionTypes';
 
-const myPageReducer = (state = { created: [], applied: [] }, action) => {
+const myPageReducer = (state = { created: [], applied: [], created: [] }, action) => {
     switch (action.type) {
         case FETCH_CREATED:
             return { ...state, created: action?.data };
         case FETCH_APPLIED:
             return {...state, applied: action?.data};
+        case FETCH_ACCEPTED:
+            return {...state, created: action?.data};
         default:
             return state;
     }
